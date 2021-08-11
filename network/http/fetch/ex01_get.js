19c2d4baaa9b54e11ff995c8219f45c5fe2aa982
gbox3d <gbox3d@gmail.com>
2021-08-11 23:50:13 +0900

    .....

---
+++ network/http/fetch/ex01_get.js
@@ -0,0 +1,23 @@
+// const fetch = require('node-fetch');
+import fetch from 'node-fetch'
+
+console.log('fetch exam');
+
+(async function () {
+
+    let _res = await (await fetch(`http://localhost:8080/echo?msg=hey`)).text()
+
+    console.log(_res);
+
+    // let _ = await (await (fetch(`http://localhost:8080/post/test`, {
+    //     method: 'POST',
+    //     body: 'hello',
+    //     //브라우져에서는 new Header객체를 사용함
+    //     headers: {
+    //         'Content-Type': 'text/plain'
+    //     } // 이 부분은 따로 설정하고싶은 header가 있다면 넣으세요
+    // }))).json();
+    // console.log(_)
+
+})();
+
