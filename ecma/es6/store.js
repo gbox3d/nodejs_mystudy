8d55daf57aafa4e19dfeca742a28d69a65ea701c
gbox3d <gbox3d@gmail.com>
2021-08-29 14:40:13 +0900

    duex asmple

---
+++ ecma/es6/store.js
@@ -0,0 +1,16 @@
+console.log('theApp check point 1')
+
+const store = {
+    name : '',
+    count : 0
+}
+
+
+export default store
+
+// export default function createApp(state) {
+//     return {
+        
+//     }
+// }
+
